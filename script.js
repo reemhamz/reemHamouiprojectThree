ed15cd92b675b1b320818f9f0f457ac9b2d569bd
Reem H <reemhamoui92@gmail.com>
2019-11-16 16:33:44 -0500

    edited barista :coffee:

--- script.js
+++ script.js
@@ -4,14 +4,14 @@ $(document).ready(function () {
     
 
         
-        // landing page to show and hide everything else
-            $('.landing').show();
-        $('.mainPage').hide();
+        // // landing page to show and hide everything else
+        //     $('.landing').show();
+        // $('.mainPage').hide();
             
-            //on button click, landing page disappears and the rest of the content shows
-            $('button').on('click', function () {
-                $('.landing').hide(1000)
-                $('.mainPage').show()
+        //     //on button click, landing page disappears and the rest of the content shows
+        //     $('button').on('click', function () {
+        //         $('.landing').hide(1000)
+        //         $('.mainPage').show()
 
                 let typed = new Typed('#typed', {
                     strings: ['Hello, and welcome to Starbucks!', 'We\'re currently serving seasonal drinks.', 'I can help you narrow down a few options, if you\'d like!', 'Just press the start button and we can get the coffee grinding while we narrow down your options.'],
